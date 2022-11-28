1fabde06c202a47d61de00dfb8fff3f05e0085d1
chrismenke45 <chrismenke45@gmail.com>
2022-11-28 15:02:24 -0800

    updated flight/airport association

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,8 +1,9 @@
 class Airport < ApplicationRecord
-  validates :code, presence: true, uniqueness: true, length: 3
+  validates :code, presence: true, uniqueness: true, length: { is: 3 }
+  validates :name, presence: true, length: { in: 2..150 }
   validates :city, presence: true, length: { in: 2..40 }
   validates :country, presence: true, length: { in: 2..40 }
 
-  has_many :arrival_flights, foreign_key: "arrival_airport_id", class_name: "Flight", inverse_of: "arrival_airport"
-  has_many :departure_flights, foreign_key: "departure_airport_id", class_name: "Flight", inverse_of: "departure_airport"
+  has_many :arrival_flights, foreign_key: "arrival_airport_id", class_name: "Flight", inverse_of: "arrival_airport", dependent: :destroy
+  has_many :departure_flights, foreign_key: "departure_airport_id", class_name: "Flight", inverse_of: "departure_airport", dependent: :destroy
 end
