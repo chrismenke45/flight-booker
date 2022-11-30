6c847d9978abfd2a498afb8adfc4ba2a07f4f3e6
chrismenke45 <chrismenke45@gmail.com>
2022-11-29 17:35:35 -0800

    search for flights implimented

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,17 +2,33 @@ class FlightsController < ApplicationController
   before_action :set_flight, only: %i[ show ]
 
   def index
+    @date_options = Flight.distinct.order(date: :asc).pluck("date(arrival_time)")
+    @airport_options = Airport.all.order(:code).map { |a| [a.code, a.id] }
+    if params[:flight]
+      @flights = Flight.includes(:arrival_airport, :departure_airport).where("departure_airport_id = ? AND arrival_airport_id = ?", flight_params[:departure_airport], flight_params[:arrival_airport]).day_of(DateTime.iso8601(flight_params[:date])).limit(50)
+      @flight = Flight.new
+    else
+      @flight = Flight.new
+      @flights = []
+    end
   end
 
   def show
   end
+
+  private
 
   def set_flight
     @flight = Flight.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
-  #def event_params
-  # params.require(:flight).permit(:name, :description, :date)
+  def flight_params
+    params.require(:flight).permit(:departure_airport, :arrival_airport, :date, :passengers)
+  end
+
+  #def flight_params
+  # flight_params.each do |param|
+  #  puts "yeeh"
   #end
 end
