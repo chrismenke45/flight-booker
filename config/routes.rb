81359b2a4d5cbb876a586a67a8a871ef465679a2
chrismenke45 <chrismenke45@gmail.com>
2022-11-30 09:30:36 -0800

    generated views/controller/routes for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :flights, only: [:index, :show]
+  resources :flights, only: [:index, :new, :show]
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "flights#index"
