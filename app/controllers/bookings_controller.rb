a0399f27d7e9245b4115d2c5c32cfaad29dcdf60
chrismenke45 <chrismenke45@gmail.com>
2022-11-30 18:49:29 -0800

    create functionality works

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,11 +7,34 @@ class BookingsController < ApplicationController
   end
 
   def create
-    puts booking_params
-    redirect_to root_url
+    #puts booking_params
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      flash.notice = "Flight #{@booking.flight.formatted_flight_number} successfully booked! A confirmation email has been sent to each passenger."
+      redirect_to booking_path(@booking.id)
+    else
+      flash.now[:alert] = ""
+      if @booking.errors[:'passengers.name'].include?("can't be blank") ||
+         @booking.errors[:'passengers.email'].include?("can't be blank")
+        flash.now[:alert] << "The highlighted fields cannot be left blank. "
+      end
+      if @booking.errors[:'passengers.email'].include?("has already been taken")
+        flash.now[:alert] << "The highlighted email address has been taken by a user with a different name."
+      end
+      @flight = Flight.find(@booking.flight_id)
+      render :new
+    end
+    #redirect_to root_url
   end
 
   def show
+    @booking = Booking.find(params[:id])
+    if @booking
+      render :show
+    else
+      flash[:alert] = 'Sorry, the booking you\'re looking for does not exist.'
+      redirect_to root_url
+    end
   end
 
   def index
